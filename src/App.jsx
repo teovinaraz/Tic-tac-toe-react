a03fb7267e848f46afd3f247b223b9dfa628b044
Teo Vinaraz <teovinaraz@gmail.com>
2024-09-04 23:16:52 -0300

    Added the board and basic functionality to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player initialName="Player 1" symbol="X"/>
-          <Player initialName="Player 2" symbol="O"/>
-
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
